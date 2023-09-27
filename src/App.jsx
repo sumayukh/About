943733805284a99bc3f78a8cf5e2e3feafb65a57
sumayukh <sumayukh.sinha@yahoo.com>
2023-09-27 13:53:27 +0530

    Fix: Hero rotation and code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import {
   Works,
   StarsCanvas,
 } from "./components";
-import heroBg from "./assets/herobg.png";
 
 const App = () => {
   return (
@@ -49,29 +48,6 @@ const App = () => {
         </div>
       </div>
     </BrowserRouter>
-    // <>
-    //   My Portfolio
-    //   {/* <div>
-    //     <a href="https://vitejs.dev" target="_blank">
-    //       <img src={viteLogo} className="logo" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://react.dev" target="_blank">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-    //   <h1>Vite + React</h1>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <p>
-    //       Edit <code>src/App.jsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p> */}
-    // </>
   );
 };
 
