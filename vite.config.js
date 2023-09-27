599a1dd28750369da4b552c64150708ae23baa4b
sumayukh <sumayukh.sinha@yahoo.com>
2023-09-27 15:37:11 +0530

    Fix: Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/About",
   build: {
     chunkSizeWarningLimit: 1000,
+    assetsDir: "assets",
   },
   server: {
     port: 5100,
